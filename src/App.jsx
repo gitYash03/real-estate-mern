ce08398b67004346ad3ea4827e326357588ce3ba
gitYash03 <141671316+gitYash03@users.noreply.github.com>
2024-01-26 20:35:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react"
+import {BrowseRouter, Routes, Route } from 'react'
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import Header from "./Components/Header"
 
-export default function App(){
+const App = () => {
   return (
-    <h1 className="text-red-100">Hello</h1>
+
+      <BrowseRouter>
+      <Header />
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowseRouter>
+
   )
 }
+
+export default App
