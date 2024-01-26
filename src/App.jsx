73ed090c03b21e31e810e79199ef4f8502ae821d
gitYash03 <yashkhot33@gmail.com>
2024-01-26 19:01:07 +0530

    created all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react"
+import {BrowseRouter, Routes, Route } from 'react'
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/profile"
 
-export default function App(){
+const App = () => {
   return (
-    <h1 className="text-red-100">Hello</h1>
+
+      <BrowseRouter>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowseRouter>
+
   )
 }
+
+export default App
